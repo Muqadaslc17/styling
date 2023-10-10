7d6a177ef3ebc4955053e9fcf8eb9e0e5c20f71d
Muqadaslc17 <147405232+Muqadaslc17@users.noreply.github.com>
2023-10-10 01:00:41 -0700

    working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
  
-import 'antd/dist/antd.css';
-
+//import 'antd/dist/antd.css';
+import '~antd/dist/antd.css';
 import { Alert } from 'antd'; 
 
 export default function App() { 
